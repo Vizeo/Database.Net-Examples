564d74da6c64accb27804ec3bf39a5708598f4e5
Christopher Woods <cwoods@bradhallfuel.com>
2018-10-25 20:09:15 -0600

    Just testing

---
+++ Crud Operations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Crud_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
