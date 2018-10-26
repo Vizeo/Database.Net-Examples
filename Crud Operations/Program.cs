40379071d65f0d6e40555054ee9ea5d22d65b6ba
Christopher Woods <cwoods@bradhallfuel.com>
2018-10-25 20:20:38 -0600

    Initial CRUD Examples

--- Crud Operations/Program.cs
+++ Crud Operations/Program.cs
@@ -1,12 +1,71 @@
 ﻿using System;
+using System.IO;
 
 namespace Crud_Operations
 {
     class Program
     {
+        private const string FILE_NAME = "c:\\temp\\Test.db";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if(File.Exists(FILE_NAME))
+            {
+                File.Delete(FILE_NAME);
+            }
+
+            Create();
+            Read();
+            Update();
+            Delete();
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+        public static void Create()
+        {
+            using (var shopContext = new ShopContext(FILE_NAME))
+            {
+                var item = shopContext.Items.CreateProxy();
+                item.Name = "Pillow";
+                item.Price = 45;
+
+                shopContext.Items.Add(item);
+
+                shopContext.SaveChanges();
+            }
+        }
+
+        public static void Read()
+        {
+            using (var shopContext = new ShopContext(FILE_NAME))
+            {                
+                var item = shopContext.Items.First(i => i.Name == "Pillow");
+                Console.WriteLine($"ID: {item.ItemId} Name: {item.Name} Price: {item.Price}");
+            }
+        }
+
+        public static void Update()
+        {
+            using (var shopContext = new ShopContext(FILE_NAME))
+            {
+                var item = shopContext.Items.First(i => i.ItemId == 1);
+                item.Price = 75;
+                shopContext.SaveChanges();
+            }
+        }
+
+        public static void Delete()
+        {
+            using (var shopContext = new ShopContext(FILE_NAME))
+            {
+                var item = shopContext.Items.First(i => i.ItemId == 1);
+
+                shopContext.Items.Remove(item);
+
+                shopContext.SaveChanges();
+            }
         }
     }
 }
---
+++ Crud Operations/ShopContext.cs
@@ -0,0 +1,18 @@
+﻿using Database.NET;
+using Database.NET.Relational;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Crud_Operations
+{
+    public class ShopContext : DbContext
+    {
+        public ShopContext(string filePath)
+            : base(filePath, TextEncodingType.ASCII)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
